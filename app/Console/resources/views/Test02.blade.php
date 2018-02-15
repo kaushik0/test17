d158e75bb8ac67f9d012137e910b2961691065b7
k787846 <k787846@gmail.com>
2018-02-15 05:21:02 -0500

    initial commit

---
+++ app/Console/resources/views/Test02.blade.php
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <h1>
+      VYSPRATECH
+    </h1>
+   
+  </body>
+</html>
---
+++ resources/views/showProfile.blade.php
@@ -0,0 +1,9 @@
+<html>
+  
+  <body>
+    
+    <h1>
+      AUGMENTED
+    </h1>
+  </body>
+</html>
